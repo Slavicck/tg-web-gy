98721a9160a1f6e9864e252311848b06f8b151f8
Slavicck <mitin85203@mail.ru>
2024-01-15 18:35:17 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import './App.css';
-
-const tg = window.Telegram.WebApp;
+import {useEffect} from "react";
+import {useTelegram} from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
+import {Route, Routes} from 'react-router-dom'
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 
 function App() {
+    const {onToggleButton, tg} = useTelegram();
 
-  const use_effect = () => {
-    tg.ready();
-  }
+    useEffect(() => {
+        tg.ready();
+    }, [])
 
-  const on_close = () => {
-    tg.close();
-  }
-
-  return (
-    <div className="App">
-      WORK
-      <button onClick={on_close}> Закрыть</button>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Routes>
+                <Route index element={<ProductList />}/>
+                <Route path={'form'} element={<Form />}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,85 @@
+import React, {useState} from 'react';
+import './ProductList.css';
+import ProductItem from "../ProductItem/ProductItem";
+import {useTelegram} from "../../hooks/useTelegram";
+import {useCallback, useEffect} from "react";
+
+const products = [
+    {id: '1', title: 'DS', price: 1000, description: '100гр'},
+    {id: '2', title: 'DS', price: 1200, description: '100гр'},
+    {id: '3', title: 'DS shot 2', price: 1100, description: '100гр'},
+    {id: '4', title: 'DS shot 8', price: 300, description: '30гр'},
+    {id: '5', title: 'DS core 3', price: 1000, description: '100гр'},
+    {id: '6', title: 'DS core 7', price: 1800, description: '200гр'},
+    {id: '7', title: 'Duft', price: 700, description: '125гр'},
+    {id: '8', title: 'Deus', price: 600, description: '125гр'},
+]
+
+const getTotalPrice = (items = []) => {
+    return items.reduce((acc, item) => {
+        return acc += item.price
+    }, 0)
+}
+
+const ProductList = () => {
+    const [addedItems, setAddedItems] = useState([]);
+    const {tg, queryId} = useTelegram();
+
+    const onSendData = useCallback(() => {
+        const data = {
+            products: addedItems,
+            totalPrice: getTotalPrice(addedItems),
+            queryId,
+        }
+        fetch('http://localhost:8000/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+    }, [addedItems])
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [onSendData])
+
+    const onAdd = (product) => {
+        const alreadyAdded = addedItems.find(item => item.id === product.id);
+        let newItems = [];
+
+        if(alreadyAdded) {
+            newItems = addedItems.filter(item => item.id !== product.id);
+        } else {
+            newItems = [...addedItems, product];
+        }
+
+        setAddedItems(newItems)
+
+        if(newItems.length === 0) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+            tg.MainButton.setParams({
+                text: `Купить ${getTotalPrice(newItems)}`
+            })
+        }
+    }
+
+    return (
+        <div className={'list'}>
+            {products.map(item => (
+                <ProductItem
+                    product={item}
+                    onAdd={onAdd}
+                    className={'item'}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default ProductList;
