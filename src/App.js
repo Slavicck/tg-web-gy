92414b08abe5ab364603ce922862c7e07dbba869
Slavicck <mitin85203@mail.ru>
2024-01-15 17:44:20 +0300

    init

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import './App.css';
 
 function App() {
 
-  // use_effect(() => {
-  //   tg.ready();
-  // }, [])
+  const use_effect = () => {
+    tg.ready();
+  }
 
-  // const on_close = () => {
-  //   tg.close();
-  // }
+  const on_close = () => {
+    tg.close();
+  }
 
   return (
     <div className="App">
       WORK
-{/* <button onClick={on_close}>Закрыть</button> */}
+      <button onClick={on_close}> Закрыть</button>
     </div>
   );
 }
-// onClick={on_close}
+
 export default App;
