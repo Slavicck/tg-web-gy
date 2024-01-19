ba215becdca840ebdeedb70826bf473703e4051c
Slavicck <mitin85203@mail.ru>
2024-01-19 06:37:23 +0300

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,84 +1,85 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './ProductList.css';
 import ProductItem from "../ProductItem/ProductItem";
-import {useTelegram} from "../../hooks/useTelegram";
-import {useCallback, useEffect} from "react";
+import { useTelegram } from "../../hooks/useTelegram";
 
-const products = [
-    {id: '1', title: 'DS', price: 1000, description: '100гр'},
-    {id: '2', title: 'DS', price: 1200, description: '100гр'},
-    {id: '3', title: 'DS shot 2', price: 1100, description: '100гр'},
-    {id: '4', title: 'DS shot 8', price: 300, description: '30гр'},
-    {id: '5', title: 'DS core 3', price: 1000, description: '100гр'},
-    {id: '6', title: 'DS core 7', price: 1800, description: '200гр'},
-    {id: '7', title: 'Duft', price: 700, description: '125гр'},
-    {id: '8', title: 'Deus', price: 600, description: '125гр'},
-]
-
-const getTotalPrice = (items = []) => {
-    return items.reduce((acc, item) => {
-        return acc += item.price
-    }, 0)
-}
 
 const ProductList = () => {
+    const [products, setProducts] = useState([]);
     const [addedItems, setAddedItems] = useState([]);
-    const {tg, queryId} = useTelegram();
+    const { tg, queryId } = useTelegram();
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await fetch('http://localhost:8000/fetch-products');
+                const data = await response.json();
+                setProducts(data.products);
+            } catch (error) {
+                console.error('Error fetching products:', error);
+            }
+        };
+
+        fetchProducts();
+    }, []);
+
+    const getTotalPrice = (items = []) => {
+        return items.reduce((acc, item) => {
+            return acc + item.price;
+        }, 0);
+    };
 
     const onSendData = useCallback(() => {
         const data = {
             products: addedItems,
             totalPrice: getTotalPrice(addedItems),
             queryId,
-        }
+        };
         fetch('http://localhost:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(data)
-        })
-    }, [addedItems])
+            body: JSON.stringify(data),
+        });
+    }, [addedItems, queryId]);
 
     useEffect(() => {
-        tg.onEvent('mainButtonClicked', onSendData)
+        tg.onEvent('mainButtonClicked', onSendData);
         return () => {
-            tg.offEvent('mainButtonClicked', onSendData)
-        }
-    }, [onSendData])
+            tg.offEvent('mainButtonClicked', onSendData);
+        };
+    }, [onSendData, tg]);
 
     const onAdd = (product) => {
-        const alreadyAdded = addedItems.find(item => item.id === product.id);
+        const alreadyAdded = addedItems.find((item) => item.id === product.id);
         let newItems = [];
 
-        if(alreadyAdded) {
-            newItems = addedItems.filter(item => item.id !== product.id);
+        if (alreadyAdded) {
+            newItems = addedItems.filter((item) => item.id !== product.id);
         } else {
             newItems = [...addedItems, product];
         }
 
-        setAddedItems(newItems)
+        setAddedItems(newItems);
 
-        if(newItems.length === 0) {
+        if (newItems.length === 0) {
             tg.MainButton.hide();
         } else {
             tg.MainButton.show();
             tg.MainButton.setParams({
-                text: `Купить ${getTotalPrice(newItems)}`
-            })
+                text: `Купить ${getTotalPrice(newItems)}`,
+            });
         }
-    }
+    };
 
     return (
         <div className={'list'}>
-            {products.map(item => (
-                <ProductItem
-                    product={item}
-                    onAdd={onAdd}
-                    className={'item'}
-                />
+            {products.map((item) => (
+                <ProductItem product={item} onAdd={onAdd} className={'item'} key={item.id} />
             ))}
         </div>
+
     );
 };
 
